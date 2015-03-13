90d278a9fc32cb7938b7f619339a3cb1e47d5fd8
m9gantz <m9gantz@gmail.com>
2015-03-13 19:22:43 +0700

    Create picfavs.js

---
+++ picfavs.js
@@ -0,0 +1,7 @@
+$('#favourites ul li a').each(function(){
+        $(this).html('<img src="'+$(this).attr('href')+'avatar.jpg" title="'+$(this).text()+'" width="30">');
+      });
+$('#favourites ul li').css({
+        'display':'inline',
+        'list-style':'none',
+});
